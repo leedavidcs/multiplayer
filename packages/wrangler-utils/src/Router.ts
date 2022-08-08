0871a5103e1fa5c1b23c90ea122e12c7500e2096
leedavidcs <lee.david.cs@gmail.com>
2022-08-08 01:12:08 -0700

    Added path merging on router

--- packages/wrangler-utils/src/Router.ts
+++ packages/wrangler-utils/src/Router.ts
@@ -11,17 +11,63 @@ export type PathParams<T extends string> =	T extends string
 				? PathParamRecord<IParamName, string[]>
 				: PathParamRecord<IFullParam>
 	: T extends `/${infer IFullPath}`
-		? IFullPath extends `${infer IPathSlug}/${infer IRestParam}`
+		? IFullPath extends `${string}/${infer IRestParam}`
 			? PathParams<`/${IRestParam}`>
 			: {}
 		: {}
 	: `Error: Could not resolve path: ${T}`
 
-export class Router {
-	public static path<TPath extends string>(
+type RouterPathHandler<T extends PathParamRecord<string, any>> = (params: T) => Promise<Response>;
+
+type RouterPathRecord<
+	TPathName extends string,
+	TPathParams extends PathParams<TPathName>
+> = Record<TPathName, RouterPathHandler<TPathParams>>;
+
+export interface RouterOptions<TPaths extends RouterPathRecord<string, any>> {
+	paths?: TPaths;
+}
+
+export class Router<TPaths extends RouterPathRecord<string, any> = {}> {
+	/**
+	 * !HACK
+	 * @description We're relying on a non-guaranteed property of objects, with
+	 * spreads to preserve the spread-order of keys for iterating. This probably
+	 * should be rewritten to use an array to ensure pattern-matching order.
+	 * @author David Lee
+	 * @date August 8, 2022
+	 */
+	public paths: TPaths = {} as TPaths;
+
+	constructor(options: RouterOptions<TPaths> = {}) {
+		this.paths = options.paths ?? {} as TPaths;
+	}
+
+	public static merge<
+		TPaths1 extends RouterPathRecord<string, any>,
+		TPaths2 extends RouterPathRecord<string, any>
+	>(
+		router1: Router<TPaths1>,
+		router2: Router<TPaths2>
+	): Router<Spread<[TPaths1, TPaths2]>> {
+		const paths1 = router1.paths;
+		const paths2 = router2.paths;
+
+		/**
+		 * TODO
+		 * @description Create an object utility to safely merge two records with proper types
+		 * @author David Lee
+		 * @date August 8, 2022
+		 */
+		return new Router({ paths: { ...paths1, ...paths2 } as any });
+	}
+
+	public path<TPath extends string>(
 		pattern: TPath,
-		handler: (params: PathParams<TPath>
-	) => Promise<void>) {
-		
+		handler: RouterPathHandler<PathParams<TPath>>
+	): Router<Spread<[TPaths, RouterPathRecord<TPath, PathParams<TPath>>]>> {
+		const newPath = { [pattern]: handler } as RouterPathRecord<TPath, PathParams<TPath>>;
+
+		return Router.merge(this, new Router({ paths: newPath }));
 	}
 }
