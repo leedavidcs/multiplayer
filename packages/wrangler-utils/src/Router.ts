922b8d382cce84086c6c9e8b977252d9eb06e7f9
leedavidcs <lee.david.cs@gmail.com>
2022-08-08 03:01:52 -0700

    Support * and + globs again

--- packages/wrangler-utils/src/Router.ts
+++ packages/wrangler-utils/src/Router.ts
@@ -9,7 +9,7 @@ export type PathParams<T extends string> =	T extends string
 	? T extends `/:${infer IFullParam}`
 		? IFullParam extends `${infer IParamName}/${infer IRestParam}`
 			? Spread<[PathParamRecord<IParamName>, PathParams<`/${IRestParam}`>]>
-			: IFullParam extends `${infer IParamName}+`
+			: IFullParam extends `${infer IParamName}${'+' | '*'}`
 				? PathParamRecord<IParamName, string[]>
 				: PathParamRecord<IFullParam>
 	: T extends `/${infer IFullPath}`
