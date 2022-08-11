dd8c35d89d032453e5031134e1cf1925f9fef57c
leedavidcs <lee.david.cs@gmail.com>
2022-08-11 00:09:16 -0700

    Added initial output type and websocket registration

--- packages/multiplayer/src/Multiplayer.ts
+++ packages/multiplayer/src/Multiplayer.ts
@@ -1,14 +1,31 @@
 export type EventData = Record<string, Json>;
 
+interface EventMessage<
+	TEvent extends string,
+	TData extends EventData
+> {
+	data: TData;
+	type: TEvent;
+}
+
 export type InputZodLike<TData extends EventData> = {
 	_input: TData;
 	parse: (data: unknown) => TData;
 }
 
+export interface EventResolverHelpers<
+	TEnv,
+	TOutput extends OutputRecord<string, any> = {}
+> {
+	broadcast: (message: OutputEventMessage<TOutput>) => void;
+	env: TEnv;
+}
+
 export type EventResolver<
 	TEnv,
-	TData extends EventData = {}
-> = (data: TData, env: TEnv) => void;
+	TData extends EventData = {},
+	TOutput extends OutputRecord<string, any> = {}
+> = (data: TData, helpers: EventResolverHelpers<TEnv, TOutput>) => void;
 
 export interface EventConfig<TEnv, TData extends EventData = {}> {
 	input: InputZodLike<TData>;
@@ -21,6 +38,20 @@ export type EventRecord<
 	TData extends EventData
 > = { [key in `${TEvent}`]: EventConfig<TEnv, TData> };
 
+export type OutputRecord<
+	TEvent extends string,
+	TData extends EventData
+> = { [key in `${TEvent}`]: TData };
+
+// Messages outputed by the server WebSockets
+type OutputEventMessage<
+	TOutput extends OutputRecord<string, any>
+> = {
+	[P in keyof TOutput]: P extends string
+		? Id<EventMessage<P, TOutput[P]>>
+		: never;
+}[keyof TOutput];
+
 export interface MultiplayerOptions<
 	TEnv,
 	TEvents extends EventRecord<TEnv, string, any>
@@ -28,7 +59,11 @@ export interface MultiplayerOptions<
 	events?: TEvents;
 }
 
-export class Multiplayer<TEnv, TEvents extends EventRecord<TEnv, string, any> = {}> {
+export class Multiplayer<
+	TEnv,
+	TOutput extends OutputRecord<string, any> = {},
+	TEvents extends EventRecord<TEnv, string, any> = {}
+> {
 	public events: TEvents;
 
 	constructor(options: MultiplayerOptions<TEnv, TEvents> = {}) {
@@ -41,7 +76,7 @@ export class Multiplayer<TEnv, TEvents extends EventRecord<TEnv, string, any> = 
 	>(
 		event: TEvent,
 		config: EventConfig<TEnv, TData>
-	): Multiplayer<TEnv, Spread<[TEvents, EventRecord<TEnv, TEvent, TData>]>> {
+	): Multiplayer<TEnv, TOutput, Spread<[TEvents, EventRecord<TEnv, TEvent, TData>]>> {
 		const newEvent = { [event]: config } as EventRecord<TEnv, TEvent, TData>;
 
 		return Multiplayer.merge(this, new Multiplayer({ events: newEvent }));
@@ -49,12 +84,13 @@ export class Multiplayer<TEnv, TEvents extends EventRecord<TEnv, string, any> = 
 
 	public static merge<
 		TEnvStatic,
+		TOutputStatic extends OutputRecord<string, any>,
 		TEventsStatic1 extends EventRecord<TEnvStatic, string, any>,
 		TEventsStatic2 extends EventRecord<TEnvStatic, string, any>
 	>(
-		multiplayer1: Multiplayer<TEnvStatic, TEventsStatic1>,
-		multiplayer2: Multiplayer<TEnvStatic, TEventsStatic2>
-	): Multiplayer<TEnvStatic, Spread<[TEventsStatic1, TEventsStatic2]>> {
+		multiplayer1: Multiplayer<TEnvStatic, TOutputStatic, TEventsStatic1>,
+		multiplayer2: Multiplayer<TEnvStatic, TOutputStatic, TEventsStatic2>
+	): Multiplayer<TEnvStatic, TOutputStatic, Spread<[TEventsStatic1, TEventsStatic2]>> {
 		const events1 = multiplayer1.events;
 		const events2 = multiplayer2.events;
 
@@ -66,8 +102,69 @@ export class Multiplayer<TEnv, TEvents extends EventRecord<TEnv, string, any> = 
 		 */
 		return new Multiplayer({ events: { ...events1, ...events2 } as any });
 	}
+
+	private static parseMessage(
+		message: MessageEvent
+	): EventMessage<string, any> | null {
+		/**
+		 * !HACK
+		 * @description We'll only handle stringified JSONs for now
+		 * @author David Lee
+		 * @date August 9, 2022
+		 */
+		if (typeof message.data !== "string") return null;
+
+		try {
+			const config = JSON.parse(message.data);
+
+			if (typeof config !== "object") return null;
+			if (typeof config.type !== "string") return null;
+			if (typeof config.data !== "object") return null;
+
+			return config as EventMessage<string, any>;
+		} catch {
+			return null;
+		}
+	}
+
+	public register(webSocket: WebSocket, env: TEnv): void {
+		webSocket.addEventListener("message", (message) => {
+			const config = Multiplayer.parseMessage(message);
+
+			/**
+			 * !HACK
+			 * @description We'll ignore any message that doesn't return a Json
+			 * for now
+			 * @author David Lee
+			 * @date August 10, 2022
+			 */
+			if (!config) return;
+
+			const eventConfig = this.events[config.type] ?? null;
+
+			if (!eventConfig) return;
+
+			const input = eventConfig.input.parse(config.data);
+
+			eventConfig.resolver(input, {
+				/**
+				 * TODO
+				 * @description Write a real broadcast method here
+				 * @author David Lee
+				 * @date August 10, 2022
+				 */
+				broadcast: () => {
+					return;
+				},
+				env
+			});
+		});
+	}
 }
 
-export const createMultiplayer = <TEnv = {}>(): Multiplayer<TEnv, {}> => {
-	return new Multiplayer<TEnv>();
+export const createMultiplayer = <
+	TEnv = {},
+	TOutput extends OutputRecord<string, any> = {}
+>(): Multiplayer<TEnv, TOutput, {}> => {
+	return new Multiplayer<TEnv, TOutput, {}>();
 };
