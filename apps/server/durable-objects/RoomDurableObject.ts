0985ab43e0e84bfa7165bde7a86d7d8ff2e0be75
leedavidcs <lee.david.cs@gmail.com>
2022-08-20 15:41:33 -0700

    Externalize router and multiplayer

--- apps/server/durable-objects/RoomDurableObject.ts
+++ apps/server/durable-objects/RoomDurableObject.ts
@@ -1,35 +1,36 @@
 import { createRouter, RequestUtils, WebSocketUtils } from "@package/wrangler-utils";
 import { createMultiplayer } from "@package/multiplayer";
 
+const multiplayer = createMultiplayer<Env, {}>();
+
+const router = createRouter<Env>()
+	.path("/websocket", (__, { env, request }) => {
+		if (!RequestUtils.isWebSocketRequest(request)) {
+			return new Response("Expected websocket", { status: 400 });
+		}
+
+		const [client, server] = WebSocketUtils.makeWebSocketPair();
+
+		const requestIp: string = RequestUtils.getRequestIp(request);
+		const limiterId = env.limiters.idFromName(requestIp);
+
+		/**
+		 * TODO
+		 * @description Connect limiter to multiplayer message event
+		 * @author David Lee
+		 * @date August 18, 2022
+		 */
+
+		multiplayer.register(server);
+
+		return new Response(null, { status: 101, webSocket: client });
+	});
+
 export class RoomDurableObject implements DurableObject {
-	private _env: Env;
-	private _multiplayer = createMultiplayer<Env, {}>();
-	private _router = createRouter<Env>()
-		.path("/websocket", (__, { request }) => {
-			if (!RequestUtils.isWebSocketRequest(request)) {
-				return new Response("Expected websocket", { status: 400 });
-			}
-
-			const [client, server] = WebSocketUtils.makeWebSocketPair();
-
-			const requestIp: string = RequestUtils.getRequestIp(request);
-			const limiterId = this._env.limiters.idFromName(requestIp);
-
-			/**
-			 * TODO
-			 * @description Connect limiter to multiplayer message event
-			 * @author David Lee
-			 * @date August 18, 2022
-			 */
-
-			this._multiplayer.register(server);
-
-			return new Response(null, { status: 101, webSocket: client });
-		});
+	private _multiplayer = multiplayer;
+	private _router = router;
 
 	constructor(state: DurableObjectState, env: Env) {
-		this._env = env;
-
 		this._multiplayer.config({
 			env,
 			storage: state.storage
