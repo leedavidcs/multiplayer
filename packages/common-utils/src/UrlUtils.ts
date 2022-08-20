8561ab0b119b34f9e300da39bd94df1ec921be05
leedavidcs <lee.david.cs@gmail.com>
2022-08-19 22:09:07 -0700

    Add UrlUtils

---
+++ packages/common-utils/src/UrlUtils.ts
@@ -0,0 +1,43 @@
+export class UrlUtils {
+	public static ensureHttp(url: string): string {
+		return `http://${UrlUtils.stripProtocol(url)}`;
+	}
+
+	public static ensureHttps(url: string): string {
+		return `https://${UrlUtils.stripProtocol(url)}`;
+	}
+
+	public static ensureWs(url: string): string {
+		return `ws://${UrlUtils.stripProtocol(url)}`;
+	}
+
+	public static ensureWss(url: string): string {
+		return `wss://${UrlUtils.stripProtocol(url)}`;
+	}
+
+	public static isValid(url: string): boolean {
+		try {
+			const _url = new URL(url);
+
+			return _url.protocol === "http:" || _url.protocol === "https:";
+		} catch {
+			return false;
+		}
+	}
+
+	public static preferHttps(url: string): string {
+		return /^(http|wss):\/\/localhost/.test(url)
+			? UrlUtils.ensureHttp(url)
+			: UrlUtils.ensureHttps(url);
+	}
+
+	public static preferWss(url: string): string {
+		return /^(http|wss):\/\/localhost/.test(url)
+			? UrlUtils.ensureWs(url)
+			: UrlUtils.ensureWss(url);
+	}
+
+	public static stripProtocol(url: string): string {
+		return url.replace(/^(wss?|https?):\/\//, "");
+	}
+}
--- packages/common-utils/src/index.ts
+++ packages/common-utils/src/index.ts
@@ -2,6 +2,7 @@ import _ms, { StringValue } from "ms";
 
 export * from "./ArrayUtils";
 export * from "./ObjectUtils";
+export * from "./UrlUtils";
 
 export const ms = _ms;
 export type MsStringValue = StringValue;
