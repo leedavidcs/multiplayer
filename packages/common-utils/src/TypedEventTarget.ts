9427650592a1c7767fadb196ac998412d8b6adfe
leedavidcs <lee.david.cs@gmail.com>
2022-08-21 22:05:25 -0700

    Added TypedEventTarget
    Added TypedEventTarget

---
+++ packages/common-utils/src/TypedEventTarget.ts
@@ -0,0 +1,32 @@
+export type TypedEvent = Event & {
+	data?: unknown;
+};
+
+export interface TypedEventListener<TEvent extends TypedEvent> extends EventListener {
+	(event: TEvent): void
+}
+
+export interface TypedEventListenerObject<TEvent extends TypedEvent> extends EventListener {
+	handleEvent(object: TEvent): void
+}
+
+export type TypedEventListenerOrEventListenerObject<TEvent extends TypedEvent> =
+	TypedEventListener<TEvent> | TypedEventListenerObject<TEvent>;
+
+export interface TypedEventTarget<TEvent extends TypedEvent> extends EventTarget {
+	addEventListener(
+		type: string,
+		callback: TypedEventListenerOrEventListenerObject<TEvent>,
+		options?: AddEventListenerOptions | boolean
+	): void;
+
+	dispatchEvent(event: TEvent): boolean;
+
+	removeEventListener(
+		type: string,
+		callback: TypedEventListenerOrEventListenerObject<TEvent>,
+		options?: EventListenerOptions | boolean
+	): void;
+}
+
+export class TypedEventTarget<TEvent extends TypedEvent> extends EventTarget {}
--- packages/common-utils/src/index.ts
+++ packages/common-utils/src/index.ts
@@ -3,6 +3,7 @@ import _ms, { StringValue } from "ms";
 export * from "./ArrayUtils";
 export * from "./LangUtils";
 export * from "./ObjectUtils";
+export * from "./TypedEventTarget";
 export * from "./UrlUtils";
 
 export const ms = _ms;
