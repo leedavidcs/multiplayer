a95f6ba279a869dfb160d27af789e7eed800c1e6
leedavidcs <lee.david.cs@gmail.com>
2022-08-27 01:18:42 -0700

    Move WebSocket method to AbstractWebSocket

--- packages/multiplayer-internal/src/AbstractWebSocket.ts
+++ packages/multiplayer-internal/src/AbstractWebSocket.ts
@@ -1,3 +1,5 @@
+import { EventMessage } from "./types";
+
 type AbstractEvent = Record<string, never>;
 type AbstractCloseEvent = AbstractEvent & {
 	readonly code: number;
@@ -32,4 +34,35 @@ export abstract class AbstractWebSocket {
 	public abstract send(
 		message: string | ArrayBuffer | ArrayBufferView
 	): void;
+
+	public handleError(
+		error: unknown,
+		message?: string
+	): void {
+		if (error instanceof Error) {
+			this.sendMessage({
+				type: "$ERROR",
+				data: {
+					message: message ?? error.message,
+					stack: error.stack ?? null
+				}
+			});
+
+			return;
+		}
+
+		this.sendMessage({
+			type: "$ERROR",
+			data: {
+				message: "Unexpected error",
+				stack: null
+			}
+		});
+	}
+
+	public sendMessage<
+		TMessage extends EventMessage<string, any> = EventMessage<string, any>
+	>(data: TMessage): void {
+		this.send(JSON.stringify(data));
+	}
 }
