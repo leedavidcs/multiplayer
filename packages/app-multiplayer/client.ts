71b37041041391ff42a36a9286c1c0c31f6f7253
leedavidcs <lee.david.cs@gmail.com>
2022-09-04 11:46:02 -0700

    Delete unused getMultiplayerClient

--- packages/app-multiplayer/client.ts
+++ packages/app-multiplayer/client.ts
@@ -1,17 +1,6 @@
-import { createClient, CreateMultiplayerClientOptions, Infer } from "@package/multiplayer-client";
+import { createClient, Infer } from "@package/multiplayer-client";
 import { z }from "zod";
 import type { MultiplayerServerInput } from "./server";
-
-export type getMultiplayerClient = CreateMultiplayerClientOptions;
-
-export const _getMultiplayerClient = (options: CreateMultiplayerClientOptions) => {
-	return createClient(options)
-		.event("RECEIVE_MESSAGE", {
-			input: z.object({
-				message: z.string()
-			})
-		});
-};
 
 const _multiplayerClient = createClient()
 	.event("RECEIVE_MESSAGE", {
